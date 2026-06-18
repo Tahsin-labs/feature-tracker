46329c647158200dbec3ec16a35622572663aa74
Tahsin Habib <your-email@example.com>
2026-06-18 21:25:18 +0600

    hit http resqust and sand responce in server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,40 @@
-import { createServer, IncomingMessage, Server } from "http";
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 
 // to create a server
-const server:Server = createServer((req:IncomingMessage ,res)=>{
-  console.log(req)
-})
+const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
 
-server.listen(5000,()=>{
+
+
+    const url = req.url
+    const method = req.method
+
+    if (url === "/" && method === "GET") {
+        // console.log("this is rout rout")
+        res.writeHead(200, { "content-type": "application/json" })   // --- this is for content type
+        res.end(JSON.stringify({ message: "this is root route" }))   //----this line for sending data, the data should stringify 
+    }
+
+    else if (url?.startsWith("/products")) {
+        res.writeHead(200, { "content-type": "application/json" })   
+        res.end(JSON.stringify({ message: "this is product route" }))
+    } else {
+        res.writeHead(404, { "content-type": "application/json" })   
+        res.end(JSON.stringify({ message: "route not found" }))   
+
+    }
+
+}
+)
+
+
+
+
+
+
+
+
+
+
+server.listen(5000, () => {
     console.log("server is running on the post 5000")
 })
