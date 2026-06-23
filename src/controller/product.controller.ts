8c41d684f0a8ac2c5926e52f0f722408c13abcd9
Tahsin Habib <your-email@example.com>
2026-06-23 21:01:38 +0600

    get mathored addet

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -8,30 +8,46 @@
 //          res.writeHead(200, { "content-type": "application/json" })
 //     res.end(JSON.stringify({ message: "this is product route", data:{} }))
 //     }
-
-
-
 // }
 
 
-import type { IncomingMessage, ServerResponse } from "http";
 
-export const productController = (req: IncomingMessage, res: ServerResponse) => {
+import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
+import type { IProduct } from "../type/product.type";
+
+export const productController = async (req: IncomingMessage, res: ServerResponse) => {
     const url = req.url;
     const method = req.method;
 
+    const urlParts = url?.split("/");
+    // console.log(urlParts)
+    const id = urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+
+
+
+    //   Get all products 
     if (url === "/products" && method === "GET") {
 
-    const products =[
-        {
-            id:1,
-            name:"product -1"
-        }
-    ]
-   
-
+        // const products =[
+        //     {
+        //         id:1,
+        //         name:"product -3"
+        //     }
+        // ]
+        const products = readProduct();
 
         res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({ message: "this is product route", data: products }));
+    }
+    
+    else if (method === "GET" && id !== null) {
+        const products = readProduct();
+        const product = products.find((p: IProduct) => p.id === id);
+        console.log(product)
+        
+         res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "products found", data: product }));
+
     }
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,7 +7,7 @@ export const routeHandler = (req: IncomingMessage, res: ServerResponse) => {
     if (url === "/" && method === "GET") {
         // console.log("this is rout rout")
         res.writeHead(200, { "content-type": "application/json" })   // --- this is for content type
-        res.end(JSON.stringify({ message: "this is root route" }))   //----this line for sending data, the data should stringify 
+        res.end(JSON.stringify({ message: "this is root route" }))   //----this for sending data, the data should stringify 
     }
     else if (url?.startsWith("/products")) {
        productController(req,res)
